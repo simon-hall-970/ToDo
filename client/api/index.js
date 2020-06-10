ddc079839df2a13146ad4ff7b6148f45f5d52f24
Simon Hall <simon970@gmail.com>
2020-06-10 14:15:50 +1200

    write api function to get task list

---
+++ client/api/index.js
@@ -0,0 +1,10 @@
+const request = require('superagent')
+
+const taskListUrl = 'http://localhost:3000/api/v1/'
+
+export function getList() {
+    return request
+    .get(taskListUrl)
+    .then(res => res.body)
+}
+
